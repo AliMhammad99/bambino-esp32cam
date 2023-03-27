b3e3d5758ab4c8820b7f64ea36fa99fcb770cf3b
Ali Mhammad <alimhmd.engineer@gmail.com>
2023-03-27 23:25:32 +0300

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,19 +1,42 @@
 #include <EEPROM.h>
 #include "BluetoothSerial.h"
 #include "WiFi.h"
-// #include <Arduino.h>
-
-
-// #include "soc/soc.h"
-// #include "soc/rtc_cntl_reg.h"
 #include "esp_camera.h"
 #include "esp_bt.h"
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h"           //disable brownout problems
+#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+#include "esp_http_server.h"
 
-#define EEPROM_SIZE 512          //Size used from the EEPROM to store ssid and password (max = 512bytes)
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+#define EEPROM_SIZE 129          //Size used from the EEPROM to store ssid and password (max = 512bytes)
 BluetoothSerial SerialBT;        //Object for Bluetooth
 unsigned long previousTime = 0;  //Used to track elapsed time
-unsigned int interval = 30000;    //Time to wait for a bluetooth connection ms (30s)
+unsigned int interval = 30000;   //Time to wait for a bluetooth connection ms (30s)
 
+
+
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+httpd_handle_t stream_httpd = NULL;
+
+String mode;
+
+/*
+esp32cam: 2 modes: local and remote
+set mode using bluetooth (add drop down to mobile app with 2 options)
+save mode to EEPROM (1byte)
+max standard ssid and password?
+retrieve from EEPROM and save in global var 
+decide which function to run (startCameraServer() or webhost)
+*/
 
 // CAMERA_MODEL_AI_THINKER
 #define PWDN_GPIO_NUM 32
@@ -61,7 +84,7 @@ void setupCamera() {
   config.pin_sscb_scl = SIOC_GPIO_NUM;
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 5000000;
+  config.xclk_freq_hz = 7000000;
   config.pixel_format = PIXFORMAT_JPEG;
   // init with high specs to pre-allocate larger buffers
   if (psramFound()) {
@@ -73,7 +96,7 @@ void setupCamera() {
     config.fb_count = 1;
   } else {
 
-    // Serial.println("NO PSRAM ----");    
+    // Serial.println("NO PSRAM ----");
     config.frame_size = FRAMESIZE_HD;
 
     config.jpeg_quality = 12;  // 0-63 lower number means higher quality
@@ -83,12 +106,12 @@ void setupCamera() {
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-    // Serial.printf("Camera init failed with error 0x%x", err);
+    Serial.printf("Camera init failed with error 0x%x", err);
     delay(1000);
     ESP.restart();
   }
-  sensor_t *s = esp_camera_sensor_get();
-  s->set_framesize(s, FRAMESIZE_HD);     // VGA|CIF|QVGA|HQVGA|QQVGA   ( UXGA? SXGA? XGA? SVGA? )
+  sensor_t* s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_HD);        // VGA|CIF|QVGA|HQVGA|QQVGA   ( UXGA? SXGA? XGA? SVGA? )
   s->set_brightness(s, 0);                  // -2 to 2
   s->set_contrast(s, 0);                    // -2 to 2
   s->set_saturation(s, 0);                  // -2 to 2
@@ -122,12 +145,120 @@ boolean blueToothTimedOut() {
   digitalWrite(RED_LED_GPIO_NUM, LOW);
   delay(1000);
   unsigned long currentTime = millis();
-  Serial.println((currentTime - previousTime)/1000);
+  Serial.println((currentTime - previousTime) / 1000);
   if (currentTime - previousTime >= interval) {
     previousTime = currentTime;
     return true;
   }
   return false;
+}
+
+static esp_err_t stream_handler(httpd_req_t* req) {
+  camera_fb_t* fb = NULL;
+  esp_err_t res = ESP_OK;
+  size_t _jpg_buf_len = 0;
+  uint8_t* _jpg_buf = NULL;
+  char* part_buf[64];
+
+  res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+  if (res != ESP_OK) {
+    return res;
+  }
+
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      res = ESP_FAIL;
+    } else {
+      if (fb->width > 400) {
+        if (fb->format != PIXFORMAT_JPEG) {
+          bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+          esp_camera_fb_return(fb);
+          fb = NULL;
+          if (!jpeg_converted) {
+            Serial.println("JPEG compression failed");
+            res = ESP_FAIL;
+          }
+        } else {
+          _jpg_buf_len = fb->len;
+          _jpg_buf = fb->buf;
+        }
+      }
+    }
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char*)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+      res = httpd_resp_send_chunk(req, (const char*)part_buf, hlen);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char*)_jpg_buf, _jpg_buf_len);
+    }
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    }
+    if (fb) {
+      esp_camera_fb_return(fb);
+      fb = NULL;
+      _jpg_buf = NULL;
+    } else if (_jpg_buf) {
+      free(_jpg_buf);
+      _jpg_buf = NULL;
+    }
+    if (res != ESP_OK) {
+      break;
+    }
+    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+  }
+  return res;
+}
+
+static esp_err_t turn_flash_on(httpd_req_t* req) {
+
+  Serial.println("Turn On");
+  digitalWrite(FLASH_LED_GPIO_NUM, 1);
+  return 200;
+}
+
+static esp_err_t turn_flash_off(httpd_req_t* req) {
+  Serial.println("Turn Off");
+  digitalWrite(FLASH_LED_GPIO_NUM, 0);
+  return 200;
+}
+
+void startCameraServer() {
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.server_port = 80;
+  config.max_open_sockets = 2;
+
+  httpd_uri_t index_uri = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = stream_handler,
+    .user_ctx = NULL
+  };
+
+  httpd_uri_t flash_on_uri = {
+    .uri = "/flash_on",
+    .method = HTTP_GET,
+    .handler = turn_flash_on,
+    .user_ctx = NULL
+  };
+
+  httpd_uri_t flash_off_uri = {
+    .uri = "/flash_off",
+    .method = HTTP_GET,
+    .handler = turn_flash_off,
+    .user_ctx = NULL
+  };
+
+
+
+  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+  if (httpd_start(&stream_httpd, &config) == ESP_OK) {
+    httpd_register_uri_handler(stream_httpd, &index_uri);
+    httpd_register_uri_handler(stream_httpd, &flash_on_uri);
+    httpd_register_uri_handler(stream_httpd, &flash_off_uri);
+  }
 }
 
 void setup() {
@@ -165,7 +296,17 @@ void setup() {
     //Save password to EEPROM
     String enteredPassword = SerialBT.readStringUntil('\n');
     enteredPassword.remove(enteredPassword.length() - 1, 1);
-    EEPROM.writeString(256, enteredPassword);
+    EEPROM.writeString(64, enteredPassword);
+    EEPROM.commit();
+
+    //Wait until recieving mode
+    while (!SerialBT.available())
+      ;
+
+    //Save mode to EEPROM
+    String enteredMode = SerialBT.readStringUntil('\n');
+    enteredMode.remove(enteredMode.length() - 1, 1);
+    EEPROM.writeString(128, enteredMode);
     EEPROM.commit();
 
     //Break the bluetooth loop
@@ -182,14 +323,19 @@ void setup() {
   // free();
   delay(2000);
 
+
+
   //Read WiFi Credentials from EEPROM
   String ssid = EEPROM.readString(0);
-  String password = EEPROM.readString(256);
+  String password = EEPROM.readString(64);
   Serial.println(ssid + " " + password);
 
+  mode = EEPROM.readString(128);
+  Serial.println("MODE: " + mode);
+
   //Try to connect to Wifi using ssid and password
-  WiFi.mode(WIFI_STA);
-  WiFi.begin((const char *)ssid.c_str(), (const char *)password.c_str());
+  // WiFi.mode(WIFI_STA);
+  WiFi.begin((const char*)ssid.c_str(), (const char*)password.c_str());
 
   if (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("WiFi Failed, Restarting...");
@@ -199,7 +345,14 @@ void setup() {
     Serial.println(ssid);
   }
 
+
+
+  Serial.print("Camera Stream Ready! Go to: http://");
+  Serial.print(WiFi.localIP());
   setupCamera();
+  if(mode=="0"){
+    startCameraServer();
+  }
 }
 
 
@@ -207,5 +360,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(mode=="1"){
+    Serial.println("REMOTE MODE");
+  }
 }
